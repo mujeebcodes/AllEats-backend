e3987685db92eb6fc9b32055451f8c76dd622472
mujeebcodes <mailmujeeb@icloud.com>
2024-05-01 22:07:29 +0100

    set up project and established database connection

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Response, Request } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => console.log("Connected to the db"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
+});
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
