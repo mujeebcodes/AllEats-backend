3429a6a2f9c93f3b03769a8afcc41d49100adec0
mujeebcodes <mailmujeeb@icloud.com>
2024-05-04 20:09:32 +0100

    implemented authorization with oauth and updating user-profile

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Response, Request } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import myUserRoutes from "./routes/myUserRoute";
 
 mongoose
   .connect(process.env.MONGODB_URI as string)
@@ -11,9 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/my/user", myUserRoutes);
+
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello" });
 });
+
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
 });
---
+++ src/routes/myUserRoute.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import myUserController from "../controllers/myUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+router.post("/", jwtCheck, myUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  myUserController.updateCurrentUser
+);
+
+export default router;
