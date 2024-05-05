746ca5e7ce755a36be930dd510965aff1e9d9206
mujeebcodes <mailmujeeb@icloud.com>
2024-05-05 16:29:16 +0100

    added prod scripts and health check

--- src/routes/myUserRoute.ts
+++ src/routes/myUserRoute.ts
@@ -5,6 +5,7 @@ import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
+router.get('/', jwtCheck, jwtParse, myUserController.getCurrentUser)
 router.post("/", jwtCheck, myUserController.createCurrentUser);
 router.put(
   "/",
