a7e9e9690ded00e69652bea7040b9b25b1df5da3
mujeebcodes <mailmujeeb@icloud.com>
2024-05-06 21:15:49 +0100

    create restaurant route added

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,18 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoutes from "./routes/myUserRoute";
+import myRestaurantRoute from "./routes/myRestaurantRoutes";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
   .connect(process.env.MONGODB_URI as string)
   .then(() => console.log("Connected to the db"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
@@ -17,6 +25,7 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoutes);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello" });
